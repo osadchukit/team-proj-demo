0200ec96771b11a48647b6a40f27d8ed98bea777
Ivan Osadchuk <osadchukit@gmail.com>
2023-01-21 16:41:51 +0100

    learn promise

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-console.log('hi');
-console.log('hi');
+const formRef = document.querySelector('.form');
+const boxRef = document.querySelector('.box');
+formRef.addEventListener('submit', onSubmit)
+
+
+const BASE_URL = `https://api.agify.io?name=`;
+
+
+
+async function featchApi(name) {
+  const resp = await fetch(`${BASE_URL}${name}`);
+    const data = await resp.json();
+    return markup(data);
+}
+
+
+function onSubmit(event) {
+    event.preventDefault()
+
+    const {
+      elements: { input_form },
+    } = event.currentTarget;
+
+    const name = input_form.value.trim();
+    featchApi(name);
+}
+
+
+function markup(data) {
+    const { age, count, name } = data;
+    
+    const markupStr = `<div>
+  age ${age}, count ${count}, name ${name}
+</div>`;
+    
+    boxRef.insertAdjacentHTML('beforeend', markupStr);
+}
