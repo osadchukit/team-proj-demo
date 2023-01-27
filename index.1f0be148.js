3805c2eb2cb86ce4c8ff9d803b42379598918a5b
osadchukit <119492029+osadchukit@users.noreply.github.com>
2023-01-27 14:46:18 +0000

    Deploying to gh-pages from @ osadchukit/team-proj-demo@5314af55da59e63bb1a592d1c9f0c96da9506c30 🚀

--- index.1f0be148.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".students"),o=document.querySelector(".box");e.addEventListener("click",(function(e){e.preventDefault(),o.classList.toggle("box_active"),console.log(o)})),console.log(e);
-//# sourceMappingURL=index.1f0be148.js.map
--- index.fea350e6.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".students"),o=document.querySelector(".box");e.addEventListener("click",(function(e){e.preventDefault(),o.classList.toggle("box_active"),console.log(o)})),console.log(e)}();
-//# sourceMappingURL=index.fea350e6.js.map
